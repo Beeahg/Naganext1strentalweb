d615e163fa2285f27226fa497f4c1b674b135f02
unknown <hoangn.ahg@gmail.com>
2024-04-08 18:24:51 +0700

    Initial commit with Start Bootstrap for Naganext

--- js/rental_func.js
+++ js/rental_func.js
@@ -6,3 +6,13 @@ function tinhToanGiaThue() {
 	 // Thực hiện các phép tính của bạn ở đây...
  }
 
+function tinhToan() {
+  // Tiến hành các phép tính của bạn ở đây
+  // ...
+
+  // Giả sử bạn có kết quả là biến `ketQua`
+  var ketQua = "Nội dung kết quả tính toán của bạn";
+
+  // Cập nhật nội dung của khu vực hiển thị kết quả
+  document.getElementById('kqTinhToan').innerHTML = ketQua;
+}
