5dad77bafe359961ed62c154c27a496a086a1e58
unknown <hoangn.ahg@gmail.com>
2024-04-08 17:30:58 +0700

    Initial commit with Start Bootstrap for Naganext

--- js/rental_func.js
+++ js/rental_func.js
@@ -5,15 +5,3 @@ function tinhToanGiaThue() {
     // ...các phép tính và cập nhật giá trị các ô input...
 }
 
-/* CSS để thêm vào file style của bạn liên quan button */
-.btnH-green {
-  background-color: #28a745; /* Màu xanh lá cho nút */
-  color: white ; /* Màu chữ trắng */
-  border: 1px solid #28a745; /* Đặt màu viền giống màu nền */
-}
-
-/* Thêm hiệu ứng khi hover nếu bạn muốn */
-.btnH-green:hover {
-  background-color: #218838; /* Màu xanh đậm hơn khi hover */
-  border-color: #1e7e34; /* Màu viền đậm hơn khi hover */
-}
