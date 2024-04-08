dd75b9df222ff7371c96b46a2b753f6d371bad0a
unknown <hoangn.ahg@gmail.com>
2024-04-09 06:22:39 +0700

    Initial commit with Start Bootstrap for Naganext

--- js/rental_func.js
+++ js/rental_func.js
@@ -29,9 +29,10 @@ function tinhToanGiaThue(event) {
  }
 
 function capNhatLabel() {
-  var kyHanThue = document.getElementById('productInput3').value; // Lấy giá trị được chọn
+  var kyHanThue = document.getElementById('productInput3').value; // Lấy giá trị được chọn (1, 2, 3)
   var labelo1 = document.getElementById('tuyChonLabel1');
-  labelo1.textContent = "TÙY CHỌN 1: THUÊ VÀ BẠN MUA LẠI SAU KHI THUÊ " + kyHanThue; // Cập nhật nội dung label option 1
   var labelo2 = document.getElementById('tuyChonLabel2');
+   
+  labelo1.textContent = "TÙY CHỌN 1: THUÊ VÀ BẠN MUA LẠI SAU KHI THUÊ " + kyHanThue; // Cập nhật nội dung label option 1
   labelo2.textContent = "TÙY CHỌN 2: HOÀN LẠI MÁY CHO NAGANEXT " + kyHanThue; // Cập nhật nội dung label option 2
 }
