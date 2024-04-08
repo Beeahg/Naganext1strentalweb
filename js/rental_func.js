f7de1c1e2693210c37045eac230f94f64dc5106b
unknown <hoangn.ahg@gmail.com>
2024-04-08 17:05:23 +0700

    Initial commit with Start Bootstrap for Naganext

--- js/rental_func.js
+++ js/rental_func.js
@@ -1,7 +1,7 @@
 
 function tinhToanGiaThue() {
     var giaMua = document.getElementById('giaMuaDuKien').value;
-	alert('Hàm tinhToanGiaThue() được gọi!');
+	alert('Giá mua dự kiến là: ' + giaMua);
     // ...các phép tính và cập nhật giá trị các ô input...
 }
 
