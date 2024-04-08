360da8f3ec1b48833d5a81a53513899378987dde
unknown <hoangn.ahg@gmail.com>
2024-04-08 18:56:03 +0700

    Initial commit with Start Bootstrap for Naganext

--- js/rental_func.js
+++ js/rental_func.js
@@ -1,12 +1,7 @@
 
 function tinhToanGiaThue() {
+	event.preventDefault(); // Thêm dòng này để ngăn việc gửi form
     var giaMua = document.getElementById('giaMuaDuKien').value;
-    // ...các phép tính và cập nhật giá trị các ô input...
-	 // Thực hiện các phép tính của bạn ở đây...
-	 // Cập nhật giá trị vào ô mong muốn
-	   // Thực hiện các phép tính của bạn ở đây...
-    // Ví dụ: giả sử bạn chỉ muốn hiển thị giá trị nhập vào mà không tính toán gì thêm
-    var giaMua = giaMuaDuKien;
     document.getElementById('giaMua').value = giaMua;
  }
 
