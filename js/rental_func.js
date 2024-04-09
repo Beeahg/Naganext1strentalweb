7bdbfd9be327e288306db9ad8bc91e98c0219058
unknown <hoangn.ahg@gmail.com>
2024-04-09 13:52:27 +0700

    Initial commit with Start Bootstrap for Naganext

--- js/rental_func.js
+++ js/rental_func.js
@@ -11,17 +11,16 @@ function tinhToanGiaThue(event) {
   var giaMua = parseFloat(document.getElementById('giaMuaDuKien').value) || 0;
   var soLuong = parseFloat(document.getElementById('soLuongDuKien').value) || 0;
   var kyHan = document.getElementById('kyHanThue').value);
-  var kyHanLabel = document.getElementById('kyHanThue'); 
 
-  var donGiaThue = giaMua * soLuong + 1000;
+  var donGiaThueo1 = giaMua * soLuong + 1000;
   var tongGiamualai = giaMua * soLuong + 2000;
   var tongGiaThueMualai = giaMua * soLuong + 3000;
   
-  var donGiaThueo2 = giaMua * soLuong + 1000;
-  var tongGiaThueo2 = giaMua * soLuong + 2000;
+  var donGiaThueo2 = giaMua * soLuong + 4000;
+  var tongGiaThueo2 = giaMua * soLuong + 5000;
   
 
-  document.getElementById('donGiaThue').value = donGiaThue.toFixed(2);
+  document.getElementById('donGiaThueo1').value = donGiaThueo1.toFixed(2);
   document.getElementById('tongGiamualai').value = tongGiamualai.toFixed(2);
   document.getElementById('tongGiaThueMualai').value = tongGiaThueMualai.toFixed(2);
   
@@ -32,8 +31,9 @@ function tinhToanGiaThue(event) {
 }
 
 function capNhatLabel() {
-   var kyHanThue = kyHanThueSelect.options[kyHanThueSelect.selectedIndex].text;
-   var kyHanThueSelect = document.getElementById('kyHanThue');
+	var kyHanThueSelect = document.getElementById('kyHanThue');
+    var kyHanThue = kyHanThueSelect.options[kyHanThueSelect.selectedIndex].text;
+
 
 
   var labelo1 = document.getElementById('tuyChonLabel1');
