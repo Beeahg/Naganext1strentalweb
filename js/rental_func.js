ed8a3eb37214a53b97808815668a1e337a861d95
unknown <hoangn.ahg@gmail.com>
2024-04-09 14:13:24 +0700

    Initial commit with Start Bootstrap for Naganext

--- js/rental_func.js
+++ js/rental_func.js
@@ -6,12 +6,13 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function tinhToanGiaThue(event) {
+  
+  console.log('Tính toán được gọi');
   event.preventDefault();
-
+  
   var giaMua = parseFloat(document.getElementById('giaMuaDuKien').value) || 0;
   var soLuong = parseFloat(document.getElementById('soLuongDuKien').value) || 0;
-  var kyHan = document.getElementById('kyHanThue').value;
-
+  
   var donGiaThueo1 = giaMua * soLuong + 1000;
   var tongGiamualai = giaMua * soLuong + 2000;
   var tongGiaThueMualai = giaMua * soLuong + 3000;
@@ -19,7 +20,6 @@ function tinhToanGiaThue(event) {
   var donGiaThueo2 = giaMua * soLuong + 4000;
   var tongGiaThueo2 = giaMua * soLuong + 5000;
   
-
   document.getElementById('donGiaThueo1').value = donGiaThueo1.toFixed(2);
   document.getElementById('tongGiamualai').value = tongGiamualai.toFixed(2);
   document.getElementById('tongGiaThueMualai').value = tongGiaThueMualai.toFixed(2);
@@ -27,18 +27,17 @@ function tinhToanGiaThue(event) {
   document.getElementById('donGiaThueo2').value = donGiaThueo2.toFixed(2);
   document.getElementById('tongGiaThueo2').value = tongGiaThueo2.toFixed(2);
   
-  capNhatLabel(event);
+  capNhatLabel();
 }
 
 function capNhatLabel() {
-	var kyHanThueSelect = document.getElementById('kyHanThue');
-    var kyHanThue = kyHanThueSelect.options[kyHanThueSelect.selectedIndex].text;
-
-
-
+  console.log('Cập nhật label được gọi');
+  var kyHanThueSelect = document.getElementById('kyHanThue');
+  var kyHanThue = kyHanThueSelect.options[kyHanThueSelect.selectedIndex].text;
+  
   var labelo1 = document.getElementById('tuyChonLabel1');
   var labelo2 = document.getElementById('tuyChonLabel2');
-
+  
   labelo1.textContent = "TÙY CHỌN 1: THUÊ VÀ BẠN MUA LẠI SAU KHI THUÊ " + kyHanThue;
   labelo2.textContent = "TÙY CHỌN 2: HOÀN LẠI MÁY CHO NAGANEXT " + kyHanThue;
 }
